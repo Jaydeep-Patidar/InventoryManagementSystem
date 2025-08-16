f11121e3643fddf49130dec58f04664563f27640
Jaydeep Patidar <jaydeeppatidarj@gmail.com>
2025-08-16 16:10:02 +0530

    backend code push 8

--- backend/inventory_backend/settings.py
+++ backend/inventory_backend/settings.py
@@ -1,20 +1,33 @@
 from pathlib import Path
 import os
 from decouple import config
-
-
+import environ
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY', default='fallback-key')
+# SECRET_KEY = config('SECRET_KEY', default='fallback-key')
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+# DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 
 
 
+
+# Initialise environ
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# Debug & Secret Key
+DEBUG = env.bool("DEBUG", default=False)
+SECRET_KEY = env("SECRET_KEY")
+
+# Allowed Hosts
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+
+# CORS
+CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
 
 
 
@@ -67,6 +80,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'inventory_backend.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'InventoryMS_DATABASE',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Jaydeep@2023#',
+#         'HOST': 'db.zfikpshzgvqlscqpwcaw.supabase.co',
+#         'PORT': '5432',
+#         'OPTIONS': {
+#             'sslmode': 'require',  # mandatory for Supabase remote
+#         }
+#     }
+# }
+
+
+
 if DEBUG:
     # Local SQLite
     DATABASES = {
@@ -84,7 +113,10 @@ else:
             'USER': config('DB_USER'),
             'PASSWORD': config('DB_PASSWORD'),
             'HOST': config('DB_HOST'),
-            'PORT': config('DB_PORT', default=5432, cast=int),
+            'PORT': config('DB_PORT'),
+            'OPTIONS': {
+                'sslmode': 'require',
+            }
         }
     }
 
@@ -104,9 +136,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+
+
+env = environ.Env()
+environ.Env.read_env()  # ye .env file load karega
+
+CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
